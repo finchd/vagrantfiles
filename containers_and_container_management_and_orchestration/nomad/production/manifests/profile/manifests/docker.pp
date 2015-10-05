35f3fe4d95922a2c3f64b43bfcd62ddd369a454c
Nick Chappell <nick@intronic.org>
2015-10-05 00:19:16 -0700

    Install Docker 1.8.2 from Docker's own repos and manage the systemd unit file so changes made to the sysconfig files are actually picked up by the Docker daeamon.

--- containers_and_container_management_and_orchestration/nomad/production/manifests/profile/manifests/docker.pp
+++ containers_and_container_management_and_orchestration/nomad/production/manifests/profile/manifests/docker.pp
@@ -1,5 +1,40 @@
 class profile::docker {
 
-  class { '::docker': }
+  #Add Docker's own Yum repo:
+  case $::osfamily {
+    'RedHat': {
+      yumrepo { 'dockerrepo':
+        descr    => 'Docker Repository',
+        baseurl  => "https://yum.dockerproject.org/repo/main/centos/7",
+        gpgcheck => 1,
+        gpgkey   => 'https://yum.dockerproject.org/gpg',
+        enabled  => 1,
+      }   
+    }   
+  }
 
+  class { 'docker':
+    version => '1.8.2',
+    package_name => 'docker-engine',
+    storage_driver => 'btrfs',
+    extra_parameters => '--graph /data',
+  } ~>
+  file { '/usr/lib/systemd/system/docker.service':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source => 'puppet:///files/docker/docker.service',
+    #notify  => Service['docker'],
+  } ~>
+  exec { 'systemd_reload_docker_unit_file':
+    user        => 'root',
+    command     => '/usr/bin/systemctl daemon-reload',
+    refreshonly => true,
+  } ~>
+  exec { 'systemd_restart_docker':
+    user        => 'root',
+    command     => '/usr/bin/systemctl restart docker.service',
+    refreshonly => true,
+  }
 }
